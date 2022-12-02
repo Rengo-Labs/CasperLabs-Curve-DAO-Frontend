3919d45becd893c5b3ae247c2d86814202723aaa
Touseef Ali <touseef.ali@scytalelabs.com>
2022-12-02 23:20:49 +0500

    UI implemented in minter screen for gauges

---
+++ src/app/components/Stats/PoolInfo.js
@@ -0,0 +1,273 @@
+import { Button, TextField } from '@material-ui/core';
+import { Checkbox, Divider, FormControlLabel, FormGroup, Slider } from '@mui/material';
+import React, { useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
+
+const PoolInfo = (props) => {
+    // STATES
+    const [canApplyBoost, setCanApplyBoost] = useState(false);
+    const [claimableTokens, setClaimableTokens] = useState(10);
+    const [gaugeRelativeWeight, setGaugeRelativeWeight] = useState(0);
+    const [mintedFormat, setMintedFormat] = useState() //this will be replaced by function
+    const [claimedRewards, setClaimedRewards] = useState(0);
+    const [gauge, setGauge] = useState({});
+    const [claimedRewardsFormat, setClaimedRewardsFormat] = useState(); //this will be replace by function
+    const [boost, setBoost] = useState(0);
+    const [currentBoost, setCurrentBoost] = useState(0);
+    const [synthsUnavailable, setSynthsUnavailable] = useState(0);
+    const [stakedBalance, setStakedBalance] = useState(0);
+    const [poolBalanceFormat, setPoolBalanceFormat] = useState(); //this will be replace by function
+    const [depositAmount, setDepositAmount] = useState(0);
+    const [depositSlider, setDepositSlider] = useState(0);
+    const [gaugeBalance, setGaugeBalance] = useState(10);
+    const [gaugeBalanceFormat, setGaugeBalanceFormat] = useState(); //this will be replace by function
+    const [withdrawAmount, setWithdrawAmount] = useState(0);
+    const [withdrawSlider, setWithdrawSlider] = useState(0);
+    const [claimableTokensFormat, setClaimableTokensFormat] = useState(10); //this will be replace by function
+    const [claimableReward, setClaimableReward] = useState(0);
+    const [claimableRewardFormat, setClaimableRewardFormat] = useState(); //this will be replace by function
+    
+
+
+    return (
+        <>
+            <div className="row no-gutters">
+                <div className="col-12 text-center text-md-left">
+                    <div>
+                        {canApplyBoost && claimableTokens == 0 ? (
+                            <div>
+                                <Button>
+                                    Apply Boost
+                                </Button>
+                            </div>
+                        ) : canApplyBoost && claimableTokens > 0 ? (
+                            <div style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                margin: '5px'
+                            }}>
+                                <span style={{color: 'green'}}>
+                                    You can apply boost by claiming CRV
+                                </span>
+                            </div>
+                        ) : null }
+                        {/* GAUGE RELATIVE WEIGHT */}
+                        <div style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            margin: '5px'
+                        }}>
+                            <span className="font-weight-bold">
+                                Gauge relative weight: &nbsp;
+                            </span>
+                            {gaugeRelativeWeight}%
+                        </div>
+                        {/* MINTED CRV */}
+                        <div style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            margin: '5px'
+                        }}>
+                            <span className="font-weight-bold">
+                                Minted CRV from this gauge: &nbsp;
+                            </span>
+                            { mintedFormat } 
+                            {/* mintedFormat here will be replaced by function returning value */}
+                        </div>
+                        {['susdv2', 'sbtc'].includes(gauge.name) && claimedRewards > 0 ? (
+                            <div style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                margin: '5px'
+                            }}>
+                                Claimed
+                                {gauge.name == 'susdv2' ? (
+                                    <span>SNX: {claimedRewardsFormat}</span>
+                                ): gauge.name == 'sbtc' ? (
+                                    <span>BPT: {claimedRewardsFormat}</span>
+                                    // ClaimedRewardsFormat has to change by function 
+                                ): null}
+                            </div>
+                        ): null}
+                        {boost !== null && !isNaN(boost) ? (
+                            <div style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                margin: '5px'
+                            }}>
+                                <span className="font-weight-bold">
+                                    Boost: &nbsp;
+                                </span>
+                                {boost}
+                            </div>
+                        ): null}
+                        {currentBoost !== null && !isNaN(currentBoost) ? (
+                            <div style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                margin: '5px'
+                            }}>
+                                <span className="font-weight-bold">
+                                    Current Boost: &nbsp;
+                                </span>
+                                {currentBoost}
+                            </div>
+                        ): null}
+                    </div>
+                    {['susdv2', 'sbtc'].includes(gauge.name) && synthsUnavailable ? (
+                        <div>
+                            Synthetix are upgrading their contract now and claiming SNX is not available
+                            {synthsUnavailable}
+                            {/* THIS HAS TO BE AN ERROR */}
+                        </div>
+                    ): null}
+                    <div>
+                        <Divider />
+                    </div>
+                    {/* POOL */}
+                    <Row style={{margin: '10px'}}>
+                        <Col>
+                            {['susdv2','sbtc'].includes(gauge.name) && stakedBalance > 0 ? (
+                                <div style={{margin: '5px'}}>
+                                    <p>
+                                        <a href="https://curve.fi/gauge.name+/withdraw">Unstake rewards</a>
+                                    </p>
+                                </div>
+                            ): null}
+                            <div style={{margin: '5px'}}>
+                                <span className="font-weight-bold">Balance: &nbsp;</span><span>{ poolBalanceFormat }</span> { gauge.name } LP token
+                            </div>
+                            <div style={{margin: '5px'}}>
+                                Amount
+                                <TextField 
+                                    label="Deposit Amount"
+                                    variant="outlined"
+                                    value={depositAmount}
+                                    style={{margin: '5px'}}
+                                />
+                            </div>
+                            {/* SLIDER PUTS OVER HERE */}
+                            <div style={{margin: '5px'}}>
+                                {depositSlider}
+                                <Slider 
+                                    defaultValue={70}                        
+                                    aria-label="Deposit Value"
+                                    valueLabelDisplay='auto'
+                                    value={depositSlider}
+                                    onChange={(event) => {
+                                        setDepositSlider(event.target.value);                            
+                                    }}
+                                />
+                            </div>
+                            <div className="row no-gutters">
+                                <div className="col-12">
+                                    <FormGroup>
+                                        <FormControlLabel
+                                        control={<Checkbox />}
+                                        label="Infinit Approval"
+                                        />
+                                    </FormGroup>
+                                </div>
+                            </div>
+                            <div style={{margin: '5px'}}>
+                                <Button
+                                    variant='contained'
+                                    size='large'
+                                    style={{backgroundColor: "#5300e8", color: "white"}}
+                                >
+                                    Deposit
+                                    {['susdv2', 'sbtc'].includes(gauge.name) ? (
+                                        <span> and stake</span>
+                                    ) : null}
+                                </Button>
+                            </div>
+                        </Col>
+                        <Col>
+                            {gaugeBalance > 0 ? (
+                                <div>
+                                    {['susdv2','sbtc'].includes(gauge.name) && stakedBalance > 0 ? (
+                                        <div>
+                                            <p>
+                                                <a href="https://curve.fi/gauge.name+/withdraw">Unstake rewards</a>
+                                            </p>
+                                        </div>
+                                    ): null}
+                                    <div style={{margin: '5px'}}>
+                                        <span className="font-weight-bold">Balance: &nbsp;</span><span>{ gaugeBalanceFormat }</span> in gauge
+                                    </div>
+                                    <div style={{margin: '5px'}}>
+                                        Amount
+                                        <TextField 
+                                            label="Withdraw Amount"
+                                            variant="outlined"
+                                            value={withdrawAmount}
+                                            style={{margin: '5px'}}
+                                        />
+                                    </div>
+                                    {/* SLIDER PUTS OVER HERE */}
+                                    <div style={{margin: '5px'}}>
+                                        {withdrawSlider}
+                                        <Slider 
+                                            defaultValue={70}                        
+                                            aria-label="Withdraw Value"
+                                            valueLabelDisplay='auto'
+                                            value={withdrawSlider}
+                                            onChange={(event) => {
+                                                setWithdrawSlider(event.target.value);                            
+                                            }}
+                                        />
+                                    </div>
+                                    <div style={{margin: '5px'}}>
+                                        <Button
+                                            variant='contained'
+                                            size='large'
+                                            style={{backgroundColor: "#5300e8", color: "white"}}
+                                        >
+                                            Withdraw
+                                        </Button>
+                                    </div>
+                                </div>
+                            ) : null}
+                        </Col>
+                    </Row>
+                    <div>
+                        <Divider />
+                    </div>
+                    <div style={{margin: '10px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                        {claimableTokens > 0 ? (
+                            <Button
+                                variant='contained'
+                                size='large'
+                                style={{backgroundColor: "#5300e8", color: "white"}}
+                            >
+                                Claim {claimableTokensFormat}% CRV
+                            </Button>
+                        ) : null}
+                        {claimableReward > 0 ? (
+                            <Button
+                                variant='contained'
+                                size='large'
+                                style={{backgroundColor: "#5300e8", color: "white"}}
+                            >
+                                Claim {claimableRewardFormat}
+                                {gauge.name == 'susdv2' ? (
+                                    <span>SNX</span>
+                                ) : gauge.name == 'sbtc' ? (
+                                    <span>BPT</span>
+                                ) : null}
+                            </Button>
+                        ) : null}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+ 
+export default PoolInfo;
--- src/app/containers/Pages/Users/DAO/Minter.js
+++ src/app/containers/Pages/Users/DAO/Minter.js
@@ -33,6 +33,7 @@ import { convertToStr } from '../../../../components/ConvertToString/ConvertToSt
 import SigningModal from "../../../../components/Modals/SigningModal";
 import Axios from "axios";
 import { makeERC20CRVDeployWasm } from "../../../../components/blockchain/MakeDeploy/MakeDeployWasm";
+import PoolInfo from "../../../../components/Stats/PoolInfo";
 // CONTENT
 
 // COMPONENT FUNCTION
@@ -55,6 +56,52 @@ const Minter = () => {
   const handleShowSigning = () => {
     setOpenSigning(true);
   };
+
+  let gauges = [
+    {
+      name: "compound",
+      hash: "32046b7f8ca95d736e6f3fc0daa4ef636d21fc5f79cd08b5e6e4fb57df9238b9"
+    },
+    {
+      name: "usdt",
+      hash: "d2cc3ac0c9c364ec0b8e969bd09eb151f9e1b57eecddb900e85abadf2332ebef"
+    },
+    {
+      name: "y",
+      hash: "493fc8e66c2f1049b28fa661c65a2668c4e9e9e023447349fc9145c82304a65a"
+    },
+    {
+      name: "busd",
+      hash: "b761da7d5ef67f8825c30c40df8b72feca4724eb666dba556b0e3f67778143e0"
+    },
+    {
+      name: "pax",
+      hash: "b761da7d5ef67f8825c30c40df8b72feca4724eb666dba556b0e3f67778143e0"
+    },
+    {
+      name: "ren",
+      hash: "bd175245e5a7fddcf1248eee5b0ee6b88aeda94bc8bbb4766a42baf5b360cc38"
+    },
+    {
+      name: "susdv2",
+      hash: "adddc432b76fabbb9ff5a694b5839065e89764c1e51df8cffdbdc34f8925876c"
+    },
+    {
+      name: "sbtc",
+      hash: "bd175245e5a7fddcf1248eee5b0ee6b88aeda94bc8bbb4766a42baf5b360cc38"
+    },
+  ]
+
+  // let gaugesNames = {
+  //   "32046b7f8ca95d736e6f3fc0daa4ef636d21fc5f79cd08b5e6e4fb57df9238b9": 'compound',
+  //   "d2cc3ac0c9c364ec0b8e969bd09eb151f9e1b57eecddb900e85abadf2332ebef": 'usdt',
+  //   "493fc8e66c2f1049b28fa661c65a2668c4e9e9e023447349fc9145c82304a65a": 'y',
+  //   "3de805e07efbc2cd9c5d323ab4fe5f2f0c1c5da33aec527d73de34a1fc9d3735": 'busd',
+  //   "b761da7d5ef67f8825c30c40df8b72feca4724eb666dba556b0e3f67778143e0": 'pax',
+  //   "bd175245e5a7fddcf1248eee5b0ee6b88aeda94bc8bbb4766a42baf5b360cc38": 'ren',
+  //   "adddc432b76fabbb9ff5a694b5839065e89764c1e51df8cffdbdc34f8925876c": 'susdv2',
+  //   "bd175245e5a7fddcf1248eee5b0ee6b88aeda94bc8bbb4766a42baf5b360cc38": 'sbtc',
+  // }
 
   // Handlers
   // async function createLockMakeDeploy(lockedAmount, unlockTime) {
@@ -611,6 +658,39 @@ const Minter = () => {
                           </div>
                         </Paper>
                       </Box>
+                      {/* GAUGE */}
+                      {gauges.map((gauge) => (
+                        <Box
+                          sx={{
+                            width: "100%",
+                          }}
+                          className="mt-4"
+                        >
+                          <Paper elevation={4}>
+                            <div className="py-5 px-4">
+                              {/* Heading */}
+                              <div className="col-12">
+                                <Typography
+                                  variant="h5"
+                                  gutterBottom
+                                  component="div"
+                                  fontWeight={900}
+                                >
+                                  {gauge.name}
+                                  {/* CRV APY: "Some value" */}
+                                </Typography>
+                              </div>
+                              {/* Pool Info */}
+                              <div className="col-12">
+                                <PoolInfo />
+                                <div className="w-100 my-3">
+                                  <Divider />
+                                </div>
+                              </div>                            
+                            </div>
+                          </Paper>
+                        </Box>
+                      ))}                      
                     </div>
                   </div>
                 </div>
