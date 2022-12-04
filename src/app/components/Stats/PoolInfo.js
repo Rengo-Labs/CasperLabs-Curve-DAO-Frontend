8737047bf4b5234e256c772b498b8377e4306eea
Touseef Ali <touseef.ali@scytalelabs.com>
2022-12-04 14:53:59 +0500

    Slider changed according to UI design

--- src/app/components/Stats/PoolInfo.js
+++ src/app/components/Stats/PoolInfo.js
@@ -2,6 +2,68 @@ import { Button, TextField } from '@material-ui/core';
 import { Checkbox, Divider, FormControlLabel, FormGroup, Slider } from '@mui/material';
 import React, { useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
+import Tooltip from '@mui/material/Tooltip';
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+
+
+function ValueLabelComponent(props) {
+    const { children, value } = props;
+  
+    return (
+      <Tooltip enterTouchDelay={0} placement="top" title={value}>
+        {children}
+      </Tooltip>
+    );
+  }
+  
+  ValueLabelComponent.propTypes = {
+    children: PropTypes.element.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+
+
+const PrettoSlider = styled(Slider)({
+    // color: '#52af77',
+    color: 'rgb(83, 0, 232)',
+    height: 8,
+    '& .MuiSlider-track': {
+      border: 'none',
+    },
+    '& .MuiSlider-thumb': {
+      height: 24,
+      width: 24,
+      backgroundColor: '#fff',
+      border: '2px solid currentColor',
+      '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+        boxShadow: 'inherit',
+      },
+      '&:before': {
+        display: 'none',
+      },
+    },
+    '& .MuiSlider-valueLabel': {
+      lineHeight: 1.2,
+      fontSize: 12,
+      background: 'unset',
+      padding: 0,
+      width: 32,
+      height: 32,
+      borderRadius: '50% 50% 50% 0',
+    //   backgroundColor: '#52af77',
+      backgroundColor: 'rgb(83, 0, 232)',
+      transformOrigin: 'bottom left',
+      transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
+      '&:before': { display: 'none' },
+      '&.MuiSlider-valueLabelOpen': {
+        transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
+      },
+      '& > *': {
+        transform: 'rotate(45deg)',
+      },
+    },
+  });
+
 
 const PoolInfo = (props) => {
     // STATES
@@ -153,13 +215,27 @@ const PoolInfo = (props) => {
                                 />
                             </div>
                             {/* SLIDER PUTS OVER HERE */}
-                            <div style={{margin: '5px'}}>
+                            {/* <div style={{margin: '5px'}}>
                                 {depositSlider}
                                 <Slider 
                                     defaultValue={70}                        
                                     aria-label="Deposit Value"
                                     valueLabelDisplay='auto'
                                     value={depositSlider}
+                                    color='secondary'
+                                    onChange={(event) => {
+                                        setDepositSlider(event.target.value);                            
+                                    }}
+                                />
+                            </div> */}
+                            <div style={{margin: '5px'}}>
+                                {depositSlider}
+                                <PrettoSlider 
+                                    defaultValue={70}                        
+                                    aria-label="Deposit Value"
+                                    valueLabelDisplay='auto'
+                                    value={depositSlider}
+                                    color='secondary'
                                     onChange={(event) => {
                                         setDepositSlider(event.target.value);                            
                                     }}
@@ -211,13 +287,27 @@ const PoolInfo = (props) => {
                                         />
                                     </div>
                                     {/* SLIDER PUTS OVER HERE */}
-                                    <div style={{margin: '5px'}}>
+                                    {/* <div style={{margin: '5px'}}>
                                         {withdrawSlider}
                                         <Slider 
                                             defaultValue={70}                        
                                             aria-label="Withdraw Value"
                                             valueLabelDisplay='auto'
                                             value={withdrawSlider}
+                                            color='secondary'
+                                            onChange={(event) => {
+                                                setWithdrawSlider(event.target.value);                            
+                                            }}
+                                        />
+                                    </div> */}
+                                    <div style={{margin: '5px'}}>
+                                        {withdrawSlider}
+                                        <PrettoSlider 
+                                            defaultValue={70}                        
+                                            aria-label="Deposit Value"
+                                            valueLabelDisplay='auto'
+                                            value={withdrawSlider}
+                                            color='secondary'
                                             onChange={(event) => {
                                                 setWithdrawSlider(event.target.value);                            
                                             }}
