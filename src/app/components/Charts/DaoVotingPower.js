a80f26d6bdfc4689a02d72e1b77c1cc6108fff26
adrees0512133526 <muhammad.adrees@scytalelabs.com>
2023-03-07 13:51:03 +0500

    Removed duplicate values from chart

--- src/app/components/Charts/DaoVotingPower.js
+++ src/app/components/Charts/DaoVotingPower.js
@@ -17,369 +17,183 @@ const DaoVotingPower = ({ chart, daoPower }) => {
 
   // Content
 
-  console.log("daoPower in chart", daoPower, chart);
-  if (chart !== undefined) {
-    let chartData = chart.splice(0, chart.length - 11)
-    console.log("chartDatachartDatachartDatachartDatachartDatachartData", chartData);
-    for (let i = 0; i < chartData.length; i++) {
+  console.log("chart:", chart);
+  console.log("daoPower:", daoPower);
+    if (chart !== undefined) {
+      let chartData = chart.splice(0, chart.length - 11)
+      // console.log("chartDatachartDatachartDatachartDatachartDatachartData", chartData);
+      for (let i = 0; i < chartData.length; i++) {
 
-      console.log("chart at i", chartData[i][0]);
-      seriesDataX.push(chartData[i][0])
-      if (isNaN(chartData[i][1])) {
-        seriesDataY.push(50)
+        // console.log("chart at i", chartData[i][0]);
+        seriesDataX.push(chartData[i][0])
+        if (isNaN(chartData[i][1])) {
+          seriesDataY.push(50)
+        }
+        else {
+          seriesDataY.push(chartData[i][1])
+        }
+
       }
-      else {
-        seriesDataY.push(chartData[i][1])
+    }
+
+    // console.log("dataX:", seriesDataX);
+    // console.log("dataY:", seriesDataY);
+
+    const fixed = seriesDataY.map((i) => {
+      return i.toFixed(3)
+    })
+    const time = seriesDataX.map((i) => {
+      return helpers.formatDateToHumanChart(i);
+    })
+
+    let axisData = [];
+    for (var i = 0; i < seriesDataX.length; i++) {
+      axisData[i] = {
+        x: time[i],
+        y: seriesDataY[i],
+      };
+    }
+    // console.log("axisData", axisData);
+
+
+    if (daoPower !== undefined) {
+
+      for (let i = 0; i < daoPower.length; i++) {
+        daoPowerX.push(daoPower[i][0])
+        daoPowerY.push(daoPower[i][1])
       }
-
     }
-  }
 
-  console.log("dataX:", seriesDataX);
-  console.log("dataY:", seriesDataY);
+    // console.log("dataXdao:", daoPowerX);
+    // console.log("dataYdao:", daoPowerY);
 
-  const fixed = seriesDataY.map((i) => {
-    return i.toFixed(3)
-  })
-  const time = seriesDataX.map((i) => {
-    return helpers.formatDateToHumanChart(i);
-  })
-
-  let axisData = [];
-  for (var i = 0; i < seriesDataX.length; i++) {
-    axisData[i] = {
-      x: time[i],
-      y: seriesDataY[i],
-    };
-  }
-  console.log("axisData", axisData);
+    const daoTime = daoPowerX.map((i) => {
+      return helpers.formatDateToHumanChart(i);
+    })
 
 
-  if (daoPower !== undefined) {
-
-    for (let i = 0; i < daoPower.length; i++) {
-      daoPowerX.push(daoPower[i][0])
-      daoPowerY.push(daoPower[i][1])
+    let daoPowerData = [];
+    for (var i = 0; i < daoPower.length; i++) {
+      daoPowerData[i] = {
+        x: daoTime[i],
+        y: daoPowerY[i],
+      };
     }
-  }
+    // console.log("daoPowerData", daoPowerData, daoPowerY.length);
 
-  console.log("dataXdao:", daoPowerX);
-  console.log("dataYdao:", daoPowerY);
+    let series = [
+      {
+        name: "My Voting Power",
+        type: "line",
+        data: axisData
 
-  const daoTime = daoPowerX.map((i) => {
-    return helpers.formatDateToHumanChart(i);
-  })
-
-
-  let daoPowerData = [];
-  for (var i = 0; i < daoPower.length; i++) {
-    daoPowerData[i] = {
-      x: daoTime[i],
-      y: daoPowerY[i],
-    };
-  }
-  console.log("daoPowerData", daoPowerData, daoPowerY.length);
-
-  let series = [
-    {
-      name: "My Voting Power",
-      type: "line",
-      data: axisData
-      //seriesDataX
-
-      // {
-      //   x: seriesDataX[0],
-      //   y: seriesDataY[0],
-      // },
-      // {
-      //   x: seriesDataX[1],
-      //   y: seriesDataY[1],
-      // },
-      // {
-      //   x: seriesDataX[2],
-      //   y: seriesDataY[2],
-      // },
-      // {
-      //   x: seriesDataX[3],
-      //   y: seriesDataY[3],
-      // },
-      // {
-      //   x: seriesDataX[4],
-      //   y: seriesDataY[4],
-      // },
-      // {
-      //   x: seriesDataX[5],
-      //   y: seriesDataY[5],
-      // },
-      // {
-      //   x: new Date("2022-05-12"),
-      //   y: [1831.43],
-      // },
-      // {
-      //   x: new Date("2022-05-13"),
-      //   y: [2261.32],
-      // },
-      // {
-      //   x: new Date("2022-05-14"),
-      //   y: [451561497.72],
-      // },
-      // {
-      //   x: new Date("2022-05-15"),
-      //   y: [451222282.92],
-      // },
-      // {
-      //   x: new Date("2022-05-16"),
-      //   y: [445795809.24],
-      // },
-      // {
-      //   x: new Date("2022-05-17"),
-      //   y: [418345769.97],
-      // },
-      // {
-      //   x: new Date("2022-05-18"),
-      //   y: [332834519.13],
-      // },
-      // {
-      //   x: new Date("2022-05-19"),
-      //   y: [130025169.05],
-      // },
-      //],
-    },
-    {
-      name: "DAO Voting Power",
-      type: "line",
-      data: daoPowerData
-      //seriesDataX
-      // {
-      //   x: new Date("2022-05-12"),
-      //   y: [1831.43],
-      // },
-      // {
-      //   x: new Date("2022-05-13"),
-      //   y: [2261.32],
-      // },
-      // {
-      //   x: new Date("2022-05-14"),
-      //   y: [451561497.72],
-      // },
-      // {
-      //   x: new Date("2022-05-15"),
-      //   y: [451222282.92],
-      // },
-      // {
-      //   x: new Date("2022-05-16"),
-      //   y: [445795809.24],
-      // },
-      // {
-      //   x: new Date("2022-05-17"),
-      //   y: [418345769.97],
-      // },
-      // {
-      //   x: new Date("2022-05-18"),
-      //   y: [332834519.13],
-      // },
-      // {
-      //   x: new Date("2022-05-19"),
-      //   y: [130025169.05],
-      // },
-
-    },
-  ];
-
-  let options = {
-    chart: {
-      id: "chart2",
-      type: "line",
-      height: 230,
-      toolbar: {
-        autoSelected: "pan",
-        show: true,
       },
-      zoom: {
-        enabled: true,
+      {
+        name: "DAO Voting Power",
+        type: "line",
+        data: daoPowerData
       },
-    },
-    colors: ["#00cc52", "#1976d2"],
-    stroke: {
-      width: 3,
-      curve: "smooth",
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    fill: {
-      opacity: 1,
-    },
-    markers: {
-      size: 0,
-    },
-    xaxis: {
-      type: "datetime",
-    },
-    yaxis: {
-      title: {
-        text: "Amount Locked",
-        //categories: [seriesDataY]
-      },
-    },
-  };
+    ];
 
-
-
-  let seriesLine = [
-    {
-      name: "My Voting Power",
-      type: "line",
-      data:
-        axisData
-      //     {
-      //       x: new Date("2022-05-12"),
-      //       y: [1831.43],
-      //     },
-      //     {
-      //       x: new Date("2022-05-13"),
-      //       y: [2261.32],
-      //     },
-      //     {
-      //       x: new Date("2022-05-14"),
-      //       y: [451561497.72],
-      //     },
-      //     {
-      //       x: new Date("2022-05-15"),
-      //       y: [451222282.92],
-      //     },
-      //     {
-      //       x: new Date("2022-05-16"),
-      //       y: [445795809.24],
-      //     },
-      //     {
-      //       x: new Date("2022-05-17"),
-      //       y: [418345769.97],
-      //     },
-      //     {
-      //       x: new Date("2022-05-18"),
-      //       y: [332834519.13],
-      //     },
-      //     {
-      //       x: new Date("2022-05-19"),
-      //       y: [130025169.05],
-      //     },
-
-    },
-    {
-      name: "DAO Voting Power",
-      type: "line",
-      data: daoPowerData
-      //     {
-      //       x: new Date("2022-05-12"),
-      //       y: [1831.43],
-      //     },
-      //     {
-      //       x: new Date("2022-05-13"),
-      //       y: [2261.32],
-      //     },
-      //     {
-      //       x: new Date("2022-05-14"),
-      //       y: [451561497.72],
-      //     },
-      //     {
-      //       x: new Date("2022-05-15"),
-      //       y: [451222282.92],
-      //     },
-      //     {
-      //       x: new Date("2022-05-16"),
-      //       y: [445795809.24],
-      //     },
-      //     {
-      //       x: new Date("2022-05-17"),
-      //       y: [418345769.97],
-      //     },
-      //     {
-      //       x: new Date("2022-05-18"),
-      //       y: [332834519.13],
-      //     },
-      //     {
-      //       x: new Date("2022-05-19"),
-      //       y: [130025169.05],
-      //     },
-
-    },
-  ];
-  let optionsLine = {
-    chart: {
-      id: "chart1",
-      height: 130,
-      type: "area",
-      brush: {
-        target: "chart2",
-        enabled: true,
-      },
-      selection: {
-        enabled: true,
-        xaxis: {
-          min: new Date(daoPowerData.length > 0 ? daoPowerData[0].x : "1 jan 2023 11:41:14").getTime(),
-          max: new Date(daoPowerData.length > 0 ? daoPowerData[daoPowerData.length - 1].x : "1 jan 2023 11:41:14").getTime(),
+    let options = {
+      chart: {
+        id: "chart2",
+        type: "line",
+        height: 230,
+        toolbar: {
+          autoSelected: "pan",
+          show: true,
+        },
+        zoom: {
+          enabled: true,
         },
       },
-      // toolbar: {
-      //   autoSelected: "pan",
-      //   show: true,
-      // },
-      // zoom: {
-      //   enabled: true,
-      // },
-    },
-    colors: ["#00cc52", "#1976d2"],
-    fill: {
-      type: "gradient",
-      gradient: {
-        opacityFrom: 0.91,
-        opacityTo: 0.1,
+      colors: ["#00cc52", "#1976d2"],
+      stroke: {
+        width: 3,
+        curve: "smooth",
       },
-    },
-    xaxis: {
-      type: "datetime",
-      tooltip: {
+      dataLabels: {
         enabled: false,
       },
-      //categories: [seriesDataY]
-    },
-    yaxis: {
-      tickAmount: 2,
-    },
-  };
+      fill: {
+        opacity: 1,
+      },
+      markers: {
+        size: 0,
+      },
+      xaxis: {
+        type: "datetime",
+      },
+      yaxis: {
+        title: {
+          text: "Amount Locked",
+          //categories: [seriesDataY]
+        },
+      },
+    };
 
-  // Handlers
 
-  // const series = [
-  //   {
-  //     name: "Temperature in Fahrenheit", //will be displayed on the y-axis
-  //     data: [seriesDataX]
-  //   }
-  // ];
-  // const options = {
-  //   chart: {
-  //     id: "line"
-  //   },
-  //   xaxis: {
-  //     categories: [seriesDataY] //will be displayed on the x-asis
-  //   }
-  // };
 
-  // const seriesLine = [
-  //   {
-  //     name: "Temperature in Fahrenheit", //will be displayed on the y-axis
-  //     data: [seriesDataY]
-  //   }
-  // ];
-  // const optionsLine = {
-  //   chart: {
-  //     id: "area"
-  //   },
-  //   xaxis: {
-  //     categories: [seriesDataX] //will be displayed on the x-asis
-  //   }
-  // };
+    let seriesLine = [
+      {
+        name: "My Voting Power",
+        type: "line",
+        data:
+          axisData
+      },
+      {
+        name: "DAO Voting Power",
+        type: "line",
+        data: daoPowerData
+
+      },
+    ];
+    let optionsLine = {
+      chart: {
+        id: "chart1",
+        height: 130,
+        type: "area",
+        brush: {
+          target: "chart2",
+          enabled: true,
+        },
+        selection: {
+          enabled: true,
+          xaxis: {
+            min: new Date(daoPowerData.length > 0 ? daoPowerData[0].x : "1 jan 2023 11:41:14").getTime(),
+            max: new Date(daoPowerData.length > 0 ? daoPowerData[daoPowerData.length - 1].x : "1 jan 2023 11:41:14").getTime(),
+          },
+        },
+        // toolbar: {
+        //   autoSelected: "pan",
+        //   show: true,
+        // },
+        // zoom: {
+        //   enabled: true,
+        // },
+      },
+      colors: ["#00cc52", "#1976d2"],
+      fill: {
+        type: "gradient",
+        gradient: {
+          opacityFrom: 0.91,
+          opacityTo: 0.1,
+        },
+      },
+      xaxis: {
+        type: "datetime",
+        tooltip: {
+          enabled: false,
+        },
+        //categories: [seriesDataY]
+      },
+      yaxis: {
+        tickAmount: 2,
+      },
+    };
+  console.log("seriesLine",seriesLine);
+  console.log("series",series);
 
   return (
     <>
